fd9ec0e89ed6654db7128f87bcc89e6bcc439d4d
iamnotahero <42923880+iamnotahero@users.noreply.github.com>
2022-02-13 22:27:49 +0800

    Update employee.php
    Fix naming to be user friendly

--- admin/maintenance/employee.php
+++ admin/maintenance/employee.php
@@ -5,7 +5,7 @@
 <?php endif;?>
 <div class="card card-outline card-primary">
 	<div class="card-header">
-		<h3 class="card-title">List of employee_data</h3>
+		<h3 class="card-title">List of Employee Data</h3>
 		<div class="card-tools">
 		<button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
 				                  		Company Type
